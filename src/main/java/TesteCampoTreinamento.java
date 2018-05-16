46d7fdec32e0bb8919592cfc8d44cc580e34e9ec
Lucas Soares <soareslssa@aer.mil.br>
2018-05-16 13:12:13 -0300

    #24 [ELEMENTOS BASICOS] TextField e TextArea

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -1,5 +1,9 @@
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteCampoTreinamento {
@@ -8,15 +12,35 @@ public class TesteCampoTreinamento {
 	 * Constante que contém o diretório raiz onde o projeto está.
 	 */
 	private static final String USER_DIR = System.getProperty("user.dir");
+	private WebDriver driver = new ChromeDriver();
 
-	@Test
-	public void teste() {
-
-		WebDriver driver = new ChromeDriver();
+	@Before
+	public void init() {
 		driver.get(USER_DIR + "/src/main/resources/componentes.html");
 
-		// driver.quit();
+	}
 
+	@Test
+	public void testeTextField() {
+
+		WebElement txtNome = driver.findElement(By.id("elementosForm:nome"));
+		txtNome.sendKeys("Teste de escrita");
+
+		Assert.assertEquals("Teste de escrita", txtNome.getAttribute("value"));
+
+		driver.quit();
+
+	}
+
+	@Test
+	public void deveInteragirComTextArea() {
+
+		WebElement txtSugestoes = driver.findElement(By.id("elementosForm:sugestoes"));
+		txtSugestoes.sendKeys("teste");
+
+		Assert.assertEquals("teste", txtSugestoes.getAttribute("value"));
+
+		driver.quit();
 	}
 
 }
